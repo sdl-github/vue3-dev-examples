eb99f37c1a6c02a92dd6f0c8df509edf97ae3e72
sdl-code <sdl-life@foxmail.com>
2023-12-07 13:54:47 +0800

    spline

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,43 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
-
-const name = ref('')
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+<script setup>
+// 导入模型解析构造函数
+import { Application } from '@splinetool/runtime'
+import { onMounted, ref } from 'vue'
+ 
+// 初始化3d模型
+const ref3d = ref(null)
+const init3dModel = () => {
+  // 实例化解析器实例
+  let spline = new Application(ref3d.value)
+  // 拉取模型
+  spline.load('https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode').then(() => {
+  // spline.load('./scene.splinecode').then(() => {
+    console.log('3D模型加载并渲染完毕')
+    spline.setZoom(1)
+    spline.addEventListener('scroll', (e) => {
+      console.log('I have been mousewheel!');
+		});
+  })
 }
+// dom节点渲染完毕再加载
+onMounted(() => {
+  init3dModel()
+})
+ 
 </script>
-
+ 
 <template>
-  <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
+  <div class="model-container">
+    <!-- 准备3D渲染节点 -->
+    <canvas class="canvas-3d" ref="ref3d" />
   </div>
 </template>
+ 
+ 
+<style scoped lang="css">
+.model-container {
+  height: 100vh;
+  background-color: black;
+  width: 100vh;
+  flex-shrink: 0;
+}
+</style>
